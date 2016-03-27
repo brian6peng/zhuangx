89811ed07c599f82166f11c836adb2196edacd97
brian <brian6peng@126.com>
2016-03-27 14:39:25 +0800

    添加linux平台package

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ const gulp = require('gulp');
 const packager = require('electron-packager');
 const path = require('path');
 
-gulp.task('build', () => {
+gulp.task('build-win', () => {
     packager({
         arch: 'x64',
         dir: path.join(__dirname, 'app'),
@@ -18,6 +18,23 @@ gulp.task('build', () => {
             ProductName: 'zhuangx',
             CompanyName: 'brian liu'
         }
+    }, (err, appPath) => {
+        if (err) {
+            console.log(err);
+        }
+    });
+});
+
+gulp.task('build-linux', () => {
+    packager({
+        arch: 'x64',
+        dir: path.join(__dirname, 'app'),
+        platform: 'linux',
+        asar: true,
+        // ignore: /(main-es6|renderer)/i,
+        overwrite: true,
+        out: path.join(__dirname, 'out'),
+        icon: path.join(__dirname, 'build', 'icon.ico'),
     }, (err, appPath) => {
         if (err) {
             console.log(err);
